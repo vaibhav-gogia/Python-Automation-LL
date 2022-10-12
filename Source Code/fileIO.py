bd3c89cc11b9705edd7657b333811689e4790f2a
vaibhav-gogia <61176566+vaibhav-gogia@users.noreply.github.com>
2022-10-12 12:37:35 -0400

    Updates

--- Source Code/fileIO.py
+++ Source Code/fileIO.py
@@ -1,14 +1,31 @@
-fileName = 'inputFile.txt'
-f = open(fileName,'r')
-output1 = "PassFile.txt"
-output2 = "FailFile.txt"
-passFile = open(output1,'w')
-failFile = open(output2, 'w')
+# fileName = 'inputFile.txt'
+# f = open(fileName,'r')
+# output1 = "PassFile.txt"
+# output2 = "FailFile.txt"
+# passFile = open(output1,'w')
+# failFile = open(output2, 'w')
+# for line in f:
+#     line_split = line.split()
+#     if line_split[2] == "P":
+#         passFile.write(line)
+#     # else:
+#         failFile.write(line)
+# passFile.close()
+# failFile.close()
+
+import linecache
+
+
+f = open('inputFile.txt', 'r')
+passFile = open ('PassFile.txt', 'w')
+failFile = open('FailFile.txt', 'w')
+count = 0
 for line in f:
     line_split = line.split()
-    if line_split[2] == "P":
+    if line_split[2] == 'P':
         passFile.write(line)
     else:
-        failFile.write(line)
+        failFile.write(line)       
+f.close()
 passFile.close()
 failFile.close()
